269387f2a4f93ae3d7693cac234d79ebc07d95f8
kari-osk <karinaosuka@gmail.com>
2022-12-11 15:36:11 -0300

    Alter invalid message

--- ctd-esp-final/src/features/quote/constants.ts
+++ ctd-esp-final/src/features/quote/constants.ts
@@ -5,5 +5,5 @@ export enum STATE_FETCH {
 }
 
 export const LOADING_MSG = "CARREGANDO...";
-export const INVALID_NAME = "Por favor, indique um nome válido. Exemplo: Homer, Bart, Lisa, Maggie, Marge, Milhouse, Moe, Abe, Troy, Mr. Burns, Ralph, Skinner ... ";
+export const INVALID_NAME = "Por favor, indique um nome válido.";
 export const NOT_FOUND = "Nenhuma citação encontrada.";
